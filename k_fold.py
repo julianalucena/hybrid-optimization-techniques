4d958e630b73681a8cdc4ca70625bc42d088fec2
Juliana Lucena <julianalucenaa@gmail.com>
2011-04-03 21:37:08 -0300

    Added stratified k-fold.

--- k_fold.py
+++ k_fold.py
@@ -9,8 +9,8 @@ Do the separation into k-folds
 
 __author__ = 'Juliana Medeiros de Lucena (jml) and Tiago Ferreira Lima (tfl2)'
 
-def k_fold(path):
-  folds = [open('sub_training_%i' % i, 'w') for i in range(0, 5) ]
+def k_fold(path, k):
+  folds = [open('sub_training_%i' % i, 'w') for i in range(0, k) ]
   insts = open(path).readlines()
 
   while len(insts) != 0:
@@ -20,5 +20,19 @@ def k_fold(path):
       else:
         break
 
+def stratified_k_fold(path, k):
+  folds = [open('sub_training_%i' % i, 'w') for i in range(0, k) ]
+  insts = open(path).readlines()
+  qtd_insts = len(insts)/len(folds)
+  leftover = len(insts) - k*qtd_insts
+
+  for fold in folds:
+    for i in range(0, qtd_insts):
+      fold.write(insts.pop(random.randint(0, len(insts) - 1)))
+
+  for i in range(0, leftover):
+    folds[random.randint(0, len(insts) -1)].write(insts.pop(i))
+
 if __name__ == '__main__':
-  k_fold('datasets/liver/bupa.data')
+  k_fold('datasets/liver/bupa.data', 5)
+  stratified_k_fold('datasets/liver/bupa.data', 5)
