0f09fccde0f2d55c4c63670e035a700f3030208b
Juliana Lucena <julianalucenaa@gmail.com>
2011-05-22 20:47:48 -0300

    Changed stratified_k_fold function to be generic.

--- k_fold.py
+++ k_fold.py
@@ -2,6 +2,7 @@
 #-*- coding:utf-8 -*-
 
 import random
+import operator
 
 from data_manager import *
 
@@ -22,44 +23,37 @@ def k_fold(path, k):
       else:
         break
 
-# (Unfortunately) very specific to Liver dataset
 def stratified_k_fold(path, k):
-  folds = [{'1': [], '2': []}, {'1': [], '2': []}, {'1': [], '2': []},
-      {'1': [], '2': []}, {'1': [], '2': []}]
-  #folds = [open('sub_training_%i' % i, 'w') for i in range(0, k) ]
-  #insts = open(path).readlines()
   insts = get_data(path)
-  one = 142 # 42% 29 instances
-  two = 199 # 58% 39 instances
-  #map(len, [insts[klass]for klass in insts.keys()])
-  qtd_insts = 341/len(folds)
-  one_qtd = 28
-  two_qtd = 39
-
+  folds = [dict.fromkeys(insts.keys()) for i in range(0, k)]
+  # Initializing because fromkeys() uses the reference
   for fold in folds:
-    for i in range(0, one_qtd):
-      fold['1'].append(insts['1'].pop(random.randint(0, len(insts['1']) - 1)))
-      #fold.write(insts.pop(random.randint(0, len(insts) - 1)))
-    for i in range(0, two_qtd):
-      fold['2'].append(insts['2'].pop(random.randint(0, len(insts['2']) - 1)))
+    for klass in fold.keys():
+      fold[klass] = []
 
-  leftover_one = len(insts['1'])
-  leftover_two = len(insts['2'])
-  for i in range(0, leftover_one):
-    print map(lambda fold: (len(fold['1']) + len(fold['2'])), folds)
-    f = sorted(folds, key=lambda fold: (len(fold['1']) + len(fold['2'])))[0]
-    print len(fold['1']) + len(fold['2'])
-    f['1'].append(insts['1'].pop(random.randint(0, len(insts['1']) - 1)))
+  qtt_per_klass = map(len, [insts[klass] for klass in insts.keys()])
+  qtt_per_fold = map(operator.idiv, qtt_per_klass, [k] * len(qtt_per_klass))
+  qtt_leftover = map(operator.mod, qtt_per_klass, [k] * len(qtt_per_klass))
 
-  for i in range(0, leftover_two):
-    f = sorted(folds, key=lambda fold: (len(fold['1']) + len(fold['2'])))[0]
-    f['2'].append(insts['2'].pop(random.randint(0, len(insts['2']) - 1)))
+  # Distributing instances to folds
+  klass_qtt_per_fold = dict(zip(insts.keys(), qtt_per_fold))
+  for fold in folds:
+    for (k, q) in klass_qtt_per_fold.items():
+      for i in range(0, q):
+        fold[k].append(insts[k].pop(random.randint(0, len(insts[k]) - 1)))
 
+  # Distributing leftover instances to folds (inexact division)
+  klass_qtt_leftover = dict(zip(insts.keys(), qtt_leftover))
+  for (k, q) in klass_qtt_leftover.items():
+    for i in range(0, q):
+      f = sorted(folds, key=lambda fold: len(fold[k]))[0]
+      f[k].append(insts[k].pop(random.randint(0, len(insts[k]) - 1)))
+
+  # Printing just to be sure that all instances were used
   print insts
   for fold in folds:
-    print (len(fold['1']) + len(fold['2']))
-    push_data(fold, 'sub_training_%i' % folds.index(fold))
+    push_data(fold, 'test/sub_training_%i' % folds.index(fold))
 
 if __name__ == '__main__':
   #k_fold('datasets/liver/bupa.data', 5)
-  stratified_k_fold('datasets/liver/bupa.data', 5)
+  stratified_k_fold('datasets/ionosphere/ionosphere.data', 5)
